251f89a034064a46685e504dbfc1a7b6cabe634c
dareyes14 <dareyes14@cps.edu>
2024-12-16 11:38:55 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+st.write("Hello, World!")
+
+st.title("Title")
+st.header("Header")
+st.subheader("Sub-header")
+
+# Set the title of the web page
+st.title()
+
+# Set a header
+
+# Text input for name
+
+# Dropdown menu for selecting an option
+
+# Slider for selecting a value
+
+# Submit button
+
+# Additional information
+
+# Footer
+
